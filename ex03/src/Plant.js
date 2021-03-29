68d5fb84a400b65b8a1b3800f8f0ec04d68dfd1d
Mirela <adisserdarevic@gmail.com>
2021-03-29 19:03:20 +0200

    Added folder ex03

---
+++ ex03/src/Plant.js
@@ -0,0 +1,12 @@
+// src/Plant.js
+import React, { Component } from 'react';
+class Plant extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>This is a Plant Component!</h2>
+            </div>
+        )
+    }
+}
+export default Plant;
