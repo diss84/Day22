bff17011471d76021d34c25b7eede0a1cd9f40ed
Mirela <adisserdarevic@gmail.com>
2021-03-29 19:49:37 +0200

    Added folder ex06

---
+++ ex06/src/Plant.js
@@ -0,0 +1,9 @@
+import React from 'react';
+const Plant  = () => {
+        return (
+            <div className="App">
+                <h2>This is a Plant Component!</h2>
+            </div>
+        )
+    }
+export default Plant;
