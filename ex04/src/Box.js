bf7c60fb5aa8d812be5b7e9cbe93c91052e00f2a
Mirela <adisserdarevic@gmail.com>
2021-03-29 19:19:58 +0200

    Added folder ex04

---
+++ ex04/src/Box.js
@@ -0,0 +1,12 @@
+//src/Box.js
+import React, { Component } from 'react';
+class Box extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h2>This is a Box Component!</h2>
+            </div>
+        )
+    }
+}
+export default Box;
