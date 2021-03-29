22d4ac288cc748ca2826a4e9b52cf0d0ad64a15b
Mirela <adisserdarevic@gmail.com>
2021-03-29 20:28:32 +0200

    Added folder ex07

---
+++ ex07/src/App.js
@@ -0,0 +1,26 @@
+// src/App.js
+import logo from './logo.svg';
+import './App.css';
+import Box from './Box'
+function App() {
+  return (
+    <div className="App">
+       <Box />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
---
+++ ex07/src/Box.js
@@ -0,0 +1,10 @@
+//src/Box.js
+import React from 'react';
+const Box  = () => {
+    return (
+            <div className="App">
+                <h2>This is a Box Component!</h2>
+            </div>
+    )
+}
+export default Box;
